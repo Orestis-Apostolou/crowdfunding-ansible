651b613315a5cea3f9279cb9634daa6c079fa89b
Nikos Zapantis <86017435+NikosZapantis@users.noreply.github.com>
2024-12-17 15:50:51 +0200

    Spring Boot {Initialized}

---
+++ crowdfunding/crowdfunding/src/main/java/gr/hua/dit/ds/crowdfunding/DscrowdfundingApplication.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.ds.crowdfunding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscrowdfundingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscrowdfundingApplication.class, args);
+	}
+
+}
---
+++ crowdfunding/crowdfunding/src/test/java/gr/hua/dit/ds/crowdfunding/DscrowdfundingApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.ds.crowdfunding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscrowdfundingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
