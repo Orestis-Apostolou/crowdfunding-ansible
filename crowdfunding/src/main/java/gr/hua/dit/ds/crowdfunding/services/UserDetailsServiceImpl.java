ebc3346e461d35d8b153d1db4013bd9dfd2b4e8d
Orestis Apostolou <cp40061@gmail.com>
2025-01-06 20:30:07 +0200

    class name changes

--- crowdfunding/src/main/java/gr/hua/dit/ds/crowdfunding/services/UserDetailsServiceImpl.java
+++ crowdfunding/src/main/java/gr/hua/dit/ds/crowdfunding/services/UserDetailsServiceImpl.java
@@ -19,14 +19,14 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-public class UserService implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     private UserRepository userRepository;
     private RoleRepository roleRepository;
 
     private BCryptPasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder ) {
+    public UserDetailsServiceImpl(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder ) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
